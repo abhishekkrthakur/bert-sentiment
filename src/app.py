575d5589647b965d000fe822dde082e42aaa3733
abhishekkrthakur <abhishek4@gmail.com>
2020-03-24 10:18:54 +0100

    use memory

--- src/app.py
+++ src/app.py
@@ -1,18 +1,33 @@
 import config
 import torch
 import flask
+import time
 from flask import Flask
 from flask import request
 from model import BERTBaseUncased
-
+import functools
 import torch.nn as nn
+import joblib
+
 
 app = Flask(__name__)
 
 MODEL = None
 DEVICE = "cuda"
+PREDICTION_DICT = dict()
+memory = joblib.Memory("../input/", verbose=0)
 
-def sentence_prediction(sentence, model):
+def predict_from_cache(sentence):
+    if sentence in PREDICTION_DICT:
+        return PREDICTION_DICT[sentence]
+    else:
+        result = sentence_prediction(sentence)
+        PREDICTION_DICT[sentence] = result
+        return result
+
+
+@memory.cache
+def sentence_prediction(sentence):
     tokenizer = config.TOKENIZER
     max_len = config.MAX_LEN
     review = str(sentence)
@@ -42,7 +57,7 @@ def sentence_prediction(sentence, model):
     token_type_ids = token_type_ids.to(DEVICE, dtype=torch.long)
     mask = mask.to(DEVICE, dtype=torch.long)
 
-    outputs = model(
+    outputs = MODEL(
         ids=ids,
         mask=mask,
         token_type_ids=token_type_ids
@@ -55,13 +70,15 @@ def sentence_prediction(sentence, model):
 @app.route("/predict")
 def predict():
     sentence = request.args.get("sentence")
-    positive_prediction = sentence_prediction(sentence, model=MODEL)
+    start_time = time.time()
+    positive_prediction = sentence_prediction(sentence)
     negative_prediction = 1 - positive_prediction
     response = {}
     response["response"] = {
         'positive': str(positive_prediction),
         'negative': str(negative_prediction),
-        'sentence': str(sentence)
+        'sentence': str(sentence),
+        'time_taken': str(time.time() - start_time)
     }
     return flask.jsonify(response)
 
