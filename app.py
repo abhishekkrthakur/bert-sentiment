5208b4d6d08611f87ea52f43d5ff3a25b51bf3d4
Akshit Jindal <bigfatgoalie@users.noreply.github.com>
2020-08-26 14:48:39 +0530

    Update app.py
    Removed extra code for padding addition
    Added argument 'truncation=True' without which PyTorch throws a lot of warnings. 
    
    Tested the changes locally. 
    Please review and approve.

--- app.py
+++ app.py
@@ -23,17 +23,12 @@ def sentence_prediction(sentence):
     review = " ".join(review.split())
 
     inputs = tokenizer.encode_plus(
-        review, None, add_special_tokens=True, max_length=max_len
+        review, None, add_special_tokens=True, max_length=max_len,pad_to_max_length=True,truncation=True
     )
 
     ids = inputs["input_ids"]
     mask = inputs["attention_mask"]
     token_type_ids = inputs["token_type_ids"]
-
-    padding_length = max_len - len(ids)
-    ids = ids + ([0] * padding_length)
-    mask = mask + ([0] * padding_length)
-    token_type_ids = token_type_ids + ([0] * padding_length)
 
     ids = torch.tensor(ids, dtype=torch.long).unsqueeze(0)
     mask = torch.tensor(mask, dtype=torch.long).unsqueeze(0)
